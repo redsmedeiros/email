52cca6c3969255f1c6c7585d10318c9b5fc87055
Rodolpho <redsmedeiros@gmail.com>
2023-05-17 10:46:15 -0300

    criando entity e enum

---
+++ src/main/java/com/serviceEmail/enums/StatusEmail.java
@@ -0,0 +1,7 @@
+package com.serviceEmail.enums;
+
+public enum StatusEmail {
+    
+    SENT,
+    ERROR;
+}
--- src/main/java/com/serviceEmail/model/Email.java
+++ src/main/java/com/serviceEmail/model/Email.java
@@ -1,5 +1,45 @@
 package com.serviceEmail.model;
 
-public class Email {
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import com.serviceEmail.enums.*;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_email")
+public class Email implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String ownerRef;
+
+    private String emailFrom;
+
+    private String emailTo;
+
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String text;
+
+    private LocalDateTime sendDateEmail;
+
+    private StatusEmail StatusEmail;
     
 }
